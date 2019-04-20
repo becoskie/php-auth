d78a6e8a408ddf97ae62c08b65a8d1c0ab4b9693
Ray Becoskie <ray@becoskie.com>
2019-04-20 06:07:19 -0500

    Set up bootstrap, jquery and connected the styles and script files

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php include("includes/connections.php")?>
+<?php include("includes/header.php"); ?>
 <?php 
     $conn = dbConnect();
-    if($conn) {
-        $message = "Good job!!!!";
+    if ($conn) {
+        $message = "Good Job!!!";
     } else {
-        $message = "Try again!!!!";
+        $message = "Try again";
     }
 ?>
 
-<?php echo($message); ?>
+    <h1><?php echo($message); ?></h1>
 
+<?php include("includes/footer.php"); ?>
