dfbedcce3e1f7eaeb35cb0cc94ad0568bbe5f8c4
Ray Becoskie <ray@becoskie.com>
2019-04-21 13:38:45 -0500

    fixed multiple function files and directories

--- index.php
+++ index.php
@@ -1,16 +1,23 @@
 <?php include("includes/connections.php")?>
+<?php include('includes/functions.php');?>
 <?php 
-    $conn = dbConnect();
-    $page_title = "Test Auth";
-    if(isset($_POST["submit"])) {
-        $data = print_r($_POST);
-    } else {
-        $data = [];
-    }
+  $test = test();
+  $username = $password = $company = $admin = "";
+  $errNum = 0;
+  $check_empty = [];
+
+  if(isset($_POST['submit'])) {
+    $username = test_input($_POST['user_name']);
+    $password = test_input($_POST['hashed_password']);
+    $company = test_input($_POST['company']);
+    $admin = test_input($_POST['admin']);
+    array_push($check_empty, $username, $password, $company, $admin);
+    print_r($check_empty);
+  }
 ?>
 <?php include("includes/header.php"); ?>
 <section class="col-4">
-    <pre><?php print_r($data); ?></pre>
+    <pre><?php print_r($check_empty); ?></pre>
 <form id="create_user_form" method="post" action="" name="user_form" onsubmit="return validateFormInput()"> 
   <div class="form-group" id="name_input">
     <label for="user_name">User Name</label>
@@ -18,7 +25,7 @@
   </div>
   <div class="form-group" id="password_input">
     <label for="user_password">Password</label>
-    <input type="password" class="form-control" id="user_password" name="user_password"  value="" placeholder="Password">
+    <input type="password" class="form-control" id="user_password" name="hashed_password"  value="" placeholder="Password">
   </div>
   <div class="form-group" id="company_input">
     <label for="company">Company</label>
