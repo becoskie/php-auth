f5de47970646d1d0e72a8fc25f129bf84600721c
Ray Becoskie <ray@becoskie.com>
2019-04-23 04:54:27 -0500

    getting user info back from db via username and password

--- includes/functions.php
+++ includes/functions.php
@@ -6,6 +6,12 @@ function mysqli_prep($string) {
     return $escaped_string;
 }
 
+function confirm_query($result_set) {
+    if (!$result_set) {
+        die("Database query failed.");
+    }
+}
+
 function test_input($data) {
     $data = trim($data);
     $data = stripslashes($data);
@@ -30,8 +36,22 @@ function generate_salt($length) {
     return $salt;
 }
 
+function find_user_by_username($username) {
+    $conn = dbConnect();
+    $safe_username = mysqli_real_escape_string($conn, $username);
+    $sql = "SELECT * FROM dev_users WHERE username = '$safe_username' LIMIT 1";
+    $user_set = $conn->query($sql);
+    confirm_query($user_set);
+    if($user = $user_set->fetch_assoc()) {
+        return $user;
+    } else {
+        return null;
+    }
+
+}
+
 function attempt_login($username, $password) {
-    $user = find_client_by_username($username);
+    $user = find_user_by_username($username);
     if ($user) {
         if (password_check($password, $user['hashed_password'])) {
         return $user;
--- index.php
+++ index.php
@@ -2,10 +2,20 @@
 <?php include('includes/functions.php');?>
 <?php 
   $page_title = "Login Page";
+  $message = "";
+  if(isset($_POST['submit'])) {
+    $user = attempt_login($_POST["username_input"], $_POST["password"]);
+    if($user) {
+      $message = "User info here";
+    } else {
+      $message = "No info here";
+    }
+  }
   ?>
 <?php include("includes/header.php"); ?>
 <section class="col-4">
 <form id="login_user" method="post" action="" name="login_form"> 
+  <h1><?php echo($message); ?></h1>
   <div class="form-group" id="username">
     <label for="username_input">User Name</label>
     <input type="text" class="form-control" id="username_input" aria-describedby="name" name="username_input" value="" placeholder="Enter User Name">
